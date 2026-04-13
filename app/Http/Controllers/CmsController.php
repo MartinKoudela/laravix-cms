595ab104d14e5a92e16d1bfe78d6c1a56bbcab05
Martin <koudela.m@icloud.com>
2026-04-13 14:12:49 +0200

    Add `show` method to `CmsController` for dynamic content rendering; update admin panel branding and colors; remove unused widget; delete favicon.

--- app/Http/Controllers/CmsController.php
+++ app/Http/Controllers/CmsController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Content;
+use App\Models\Site;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CmsController extends Controller
 {
-    //
+    public function show(Request $request, string $slug = '/'): View
+    {
+        $site = Site::where('domain', $request->getHost())->firstOrFail();
+
+        $content = Content::where('site_id', $site->id)
+            ->where('slug', ltrim($slug, '/'))
+            ->where('status', 'published')
+            ->where(function ($query) {
+                $query->whereNull('published_at')
+                    ->orWhere('published_at', '<=', now());
+            })
+            ->with(['fields', 'taxonomies'])
+            ->first();
+
+        if (! $content) {
+            throw new NotFoundHttpException();
+        }
+
+        $theme = $site->theme ?? 'default';
+
+
+        $view = "themes.{$theme}.{$content->type}.show";
+
+
+        if (! view()->exists($view)) {
+            $view = "themes.{$theme}.default";
+        }
+
+        return view($view, compact('content', 'site'));
+    }
 }
