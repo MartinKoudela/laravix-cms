72e26a24199192b7a9080eb07657bff643ec86f7
Martin <koudela.m@icloud.com>
2026-04-20 21:07:51 +0200

    Add `HasFactory` trait to `Site` model

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -12,7 +13,7 @@ use Spatie\Activitylog\Support\LogOptions;
 #[Fillable(['name', 'domain', 'theme'])]
 class Site extends Model
 {
-    use LogsActivity;
+    use HasFactory, LogsActivity;
 
     public function getActivitylogOptions(): LogOptions
     {
